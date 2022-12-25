794befb4cd4333be52ccdb19403beff42ee4ec41
lapushkov <lapushkov@yandex.ru>
2022-12-25 15:27:50 +0300

    очередное

---
+++ copy_robo.bat
@@ -0,0 +1,15 @@
+robocopy \\10.2.1.151\C$\Galaktika\res\src\_AggregateRes\  C:\Galaktika\_git\AtlanticGrupa\_AggregateRes\  /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\_MovedRes\      C:\Galaktika\_git\AtlanticGrupa\_MovedRes\      /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\_notNeed\       C:\Galaktika\_git\AtlanticGrupa\_notNeed\       /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\_prntScr\       C:\Galaktika\_git\AtlanticGrupa\_prntScr\       /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\_temp\          C:\Galaktika\_git\AtlanticGrupa\_temp\          /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\CRPT\           C:\Galaktika\_git\AtlanticGrupa\CRPT\           /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\DSQlRepBuilder\ C:\Galaktika\_git\AtlanticGrupa\DSQlRepBuilder\ /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\frm\            C:\Galaktika\_git\AtlanticGrupa\frm\            /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\inc\            C:\Galaktika\_git\AtlanticGrupa\inc\            /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\pack\           C:\Galaktika\_git\AtlanticGrupa\pack\           /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\ReportOrg\      C:\Galaktika\_git\AtlanticGrupa\ReportOrg\      /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\sqllot\         C:\Galaktika\_git\AtlanticGrupa\sqllot\         /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\VetisAPI\       C:\Galaktika\_git\AtlanticGrupa\VetisAPI\       /MIR
+robocopy \\10.2.1.151\C$\Galaktika\res\src\xml\            C:\Galaktika\_git\AtlanticGrupa\xml\            /MIR
+
