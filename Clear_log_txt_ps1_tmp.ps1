811991665ac6c23130c362f91afcf3ffc873f3f6
lapushkov@yandex.ru <https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-desktop-6.0.13-windows-x64-installer?cid=getdotnetcore>
2023-03-18 14:48:17 +0300

    Clear_log_txt_ps1_tmp.ps1

---
+++ Clear_log_txt_ps1_tmp.ps1
@@ -0,0 +1,20 @@
+﻿$limit = (Get-Date).AddDays(-30)
+$paths = @("C:\Gal_ABM_tmp\"
+,"C:\Galaktika\_CRPT"
+,"C:\Galaktika\_CRPT_API_InfoToolsLoader"
+,"C:\Galaktika\_CRPT_PICKINGREPORT_IMPORT"
+,"C:\Galaktika\_EmailPaymentDate"
+,"C:\Galaktika\_helpDesk"
+,"C:\Galaktika\_ImportWMS"
+,"C:\Galaktika\_LoadOrder"
+,"C:\Galaktika\_Mercury"
+,"C:\Galaktika\_PraData"
+,"C:\Galaktika\_RecalcSaldoMC"
+,"C:\Galaktika\_tempGalTasks"
+)
+
+# Delete files older than the $limit.
+foreach($path in $paths) {
+  Get-ChildItem -Path $path -Recurse -Force -Include *.log,*.ps1,*.txt,*.tmp | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit }  | Remove-Item -Force
+  }
+
